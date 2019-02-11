22c58f48c99eface1101a44b06368dd6a1a7164a
Sujot Kang <sujotkang@yahoo.com>
2019-02-10 18:28:23 -0800

    Updates

---
+++ precache-manifest.38babbd47ae2c8d5f7b9ce91f8e9a24a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "357545de90fb9f239dbf",
+    "url": "/sujotkang/static/css/main.f0f71bd4.chunk.css"
+  },
+  {
+    "revision": "357545de90fb9f239dbf",
+    "url": "/sujotkang/static/js/main.357545de.chunk.js"
+  },
+  {
+    "revision": "c2fa5ea3eb295de8d023",
+    "url": "/sujotkang/static/js/1.c2fa5ea3.chunk.js"
+  },
+  {
+    "revision": "b0e8455b545fb4fee671",
+    "url": "/sujotkang/static/js/runtime~main.b0e8455b.js"
+  },
+  {
+    "revision": "8042f36c272f6b0dce6cdef6769f1ff6",
+    "url": "/sujotkang/index.html"
+  }
+];
