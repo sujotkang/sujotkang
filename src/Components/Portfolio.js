048b2ca0786fe118b7df5c81332305c7398d4be7
Sujot Kang <sujotkang@yahoo.com>
2019-02-19 18:56:30 -0800

    edit portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -13,7 +13,7 @@ const Portfolio = () => {
             transitionLeave={false}>
             <div className="container">
                 <div className="square">
-                    <h3 className="title">Communication Board</h3>
+                    <h3 className="title">UCSD Communication Board</h3>
                     <p>
                         By using an iterative design approach (double diamond process) 
                         and rapid prototyping, designed and implemented an organization and 
@@ -70,8 +70,8 @@ const Portfolio = () => {
                     <p>
                         Using InVision, communicated a mobile video game that uses augmented reality 
                         for social computing. It would make a user’s smartphone into an extension of 
-                        their hand (as a weapon or tool), and would require verbal communication to win. 
-                        The concept revolves around social computing and encouraging collaboration between users.
+                        their hand (as a weapon or tool). The concept revolves around social computing 
+                        and encouraging collaboration between users.
                     </p>
                     <div className="twoButt">
                         <Button className="portButt" href="https://docs.google.com/document/d/1qIFouBSdSUGQVI1uJUPXyQy9WZITIaoVt1QcP4psEgs/edit" target="_blank">
@@ -87,8 +87,7 @@ const Portfolio = () => {
                     <p>
                         Implemented node.js, mongoDB (mLab), Express.js, HTML, CSS, Semantic UI, Cloud9, and Heroku 
                         to create and deploy a portfolio/blog website for a poet/author. Website is a work in progress 
-                        and intended as a template. Plans for improvement include using a front-end framework to better 
-                        display and organize information, and to purchase a domain name to use.
+                        and intended as a template.
                     </p>
                     <div className="twoButt">
                         <Button className="portButt" href="https://harjinderkang.herokuapp.com/" target="_blank">
